82e3e3df2ade460a76f802453df5bd6eb9073fa9
Yakushin Alexandr <valensiec08@mail.ru>
2021-10-06 20:56:39 +0300

    Accrued miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1_000;
+        int ticket1Mile = 20;
+        int accruedMiles = ticketPrice / ticket1Mile;
+        System.out.println(accruedMiles + "miles");
+
+    }
+}
